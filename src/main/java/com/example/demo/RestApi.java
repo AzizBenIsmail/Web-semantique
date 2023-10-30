b07fc2d66bcf4561d3471a542f09d7712343ca2a
Elyes Boudhina <elyes.boudhina@etudiant-fsegt.utm.tn>
2023-10-30 18:09:16 +0100

    feat: adding getVoituresEtVelos for 'Peronne'

--- src/main/java/com/example/demo/RestApi.java
+++ src/main/java/com/example/demo/RestApi.java
@@ -59,6 +59,28 @@ public class RestApi {
             return ("Error when reading model from ontology");
         }
     }
+    @GetMapping("/voiture_velo")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public String afficherVoituresEtVelos() {
+        String NS = "";
+        if (model != null) {
+
+            NS = model.getNsPrefixURI("");
+
+
+            Model inferedModel = JenaEngine.readInferencedModelFromRuleFile(model, "data/rules.txt");
+
+            OutputStream res =  JenaEngine.executeQueryFile(inferedModel, "data/query_VoitureVelo.txt");
+
+
+            System.out.println(res);
+            return res.toString();
+
+
+        } else {
+            return ("Error when reading model from ontology");
+        }
+    }
     
     
     
