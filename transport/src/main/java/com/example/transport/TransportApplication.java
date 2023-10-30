59daad1d132d7d6746e07b21525602563d162ec4
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2023-10-30 15:48:52 +0100

    Revert "Merge branch 'main' of https://github.com/WebSemantique/ScaredToCompile-5TWIN6-Transport"
    This reverts commit c53cc56254fedcb3018a91e1247edf2b65f27d3a, reversing
    changes made to 6197145f35df15c9e524123c6000749b35fb5d9b.

--- transport/src/main/java/com/example/transport/TransportApplication.java
+++ transport/src/main/java/com/example/transport/TransportApplication.java
@@ -14,13 +14,21 @@ public class TransportApplication {
 	 */
 	public static void main(String[] args) {
 		String NS = "";
+// lire le model a partir d'une ontologie
 		Model model = JenaEngine.readModel("data/hmida.owl");
 		if (model != null) {
+//lire le Namespace de l’ontologie
 			NS = model.getNsPrefixURI("");
-			Model inferedModel =JenaEngine.readInferencedModelFromRuleFile(model, "data/rules.txt");
-			System.out.println(JenaEngine.executeQueryFile(inferedModel,"data/query.txt"));
+// apply our rules on the owlInferencedModel
+			Model inferedModel =
+
+					JenaEngine.readInferencedModelFromRuleFile(model, "data/rules.txt");
+// query on the model after inference
+			System.out.println(JenaEngine.executeQueryFile(inferedModel,
+
+					"data/query.txt"));
+
 		} else {
 			System.out.println("Error when reading model from ontology");
 		}
-
 	}}
