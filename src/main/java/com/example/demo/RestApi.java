1ed6d95257eebc3d74ff6e87228acd9b6d0b9366
Amen allah Jouini <amenallah.jouini@esprit.tn>
2023-10-30 19:45:02 +0100

    camion

--- src/main/java/com/example/demo/RestApi.java
+++ src/main/java/com/example/demo/RestApi.java
@@ -104,6 +104,29 @@ public class RestApi {
             return ("Error when reading model from ontology");
         }
     }
+
+    @GetMapping("/camion")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public String afficherCamion() {
+        String NS = "";
+        if (model != null) {
+
+            NS = model.getNsPrefixURI("");
+
+
+            Model inferedModel = JenaEngine.readInferencedModelFromRuleFile(model, "data/rules.txt");
+
+            OutputStream res =  JenaEngine.executeQueryFile(inferedModel, "data/query_Camion.txt");
+
+
+            System.out.println(res);
+            return res.toString();
+
+
+        } else {
+            return ("Error when reading model from ontology");
+        }
+    }
     
 }
 
